a682504edc0697f2674206e40c181db12dad5887
Ferryawan <ferry908ardiansyah@gmail.com>
2024-03-02 17:49:09 +0700

    latihan nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { BooksModule } from './books/books.module';
+import { typeOrmConfig } from './config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(typeOrmConfig),BooksModule],
+
+})
+export class AppModule {}
